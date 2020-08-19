05697b12cc87e3f679c563be7f0d6c854f3e3a05
iraniya <naynesh@whadatime.com>
2020-08-19 18:25:18 +0530

    Weather forcasting and alert app using MVVM Architeture

---
+++ WeatherFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherFun
+//
+//  Created by iraniya on 19/08/20.
+//  Copyright © 2020 iraniya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
